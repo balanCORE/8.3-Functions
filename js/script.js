a2fe6dc6ef022378b1940f5f3842995dfbc225c3
kieUba <bartosz.gargol.dev@gmail.com>
2018-12-11 22:04:27 +0100

    working version

--- js/script.js
+++ js/script.js
@@ -1,36 +1,61 @@
 'use strict'
 
-//  Buttony
-var countF = document.getElementById('CtoF_btn');
-var countC = document.getElementById('FtoC_btn');
+var btnCtoF = document.getElementById('CtoF_btn');
+var btnFtoC = document.getElementById('FtoC_btn');
 
-// Odpowiedzi na buttony
-var ans_CtoF = document.getElementById('CtoF_out');
-var ans_FtoC = document.getElementById('FtoC_out');
+var resultBox = document.getElementById('output');
 
-// Wykorzystanie buttonów
 
-// Funkcje przeliczające
-var tempC;
-var tempF;
-
-var CtoF = function(tempF) {
-	tempC = (tempF-32)/1.8;
-	ans_CtoF.innerHTML = tempC;
+function CtoF(tempC) {
+	var tempF = tempC * 1.8 +32;
+	return tempF + " Faranhajt";
 };
 
-var FtoC = function(tempC) {
-	tempF = tempC*1.8+32;
+
+function waterStatusC(temp) {
+	if (temp < 0) {return "woda jest w stanie stałym"}
+	else if (temp > 0 && temp < 100) {return "woda jest w stanie płynnym"}
+	else {return "woda jest w stanie lotnym"}
 };
 
-// wykorzystanie butonów
+function waterStatusF(temp) {
+	return waterStatusC((temp - 32) / 1.8)
+};
 
-countF.addEventListener('click', function() {
-	tempF = prompt('Podaj proszę temperaturę w Celsjuszach');
+
+function FtoC(tempF) {
+	var tempC = ( tempF - 32 ) / 1.8;
+	return tempC + " Celsjusz"
+};
+
+function showResult(info) {
+	resultBox.innerHTML = "<div>" + info + "</div>" + "<br>" + resultBox.innerHTML;
+};
+
+
+
+btnCtoF.addEventListener('click', function(){
+
+	var tempC = prompt('Proszę podać jaką wartość przeliczyć na stopnie Faranhajt?');
+
+	if ( !isNaN(parseInt(tempC) )) {
+		showResult(CtoF(tempC) + " " + waterStatusC(tempC));	
+	} else {
+		showResult('proszę o podanie wartości liczbowej');
+	}
+
+	
+});
+
+btnFtoC.addEventListener('click', function(){
+
+	var tempF = prompt('Proszę podać jaką wartość przeliczyć na stopnie Faranhajt?');
 
 	if ( !isNaN(parseInt(tempF) )) {
-		CtoF();
+		showResult(FtoC(tempF) + " " + waterStatusF(tempF));	
 	} else {
-		ans_CtoF.innerHTML = "liczbę proszę";
+		showResult('proszę o podanie wartości liczbowej');
 	}
+
+	
 });

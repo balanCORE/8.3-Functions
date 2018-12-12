e67613f65c4b366a7878d89661d0cc2eda340cd1
kieUba <bartosz.gargol.dev@gmail.com>
2018-12-12 10:29:53 +0100

    final

--- js/script.js
+++ js/script.js
@@ -8,14 +8,14 @@ var resultBox = document.getElementById('output');
 
 function CtoF(tempC) {
 	var tempF = tempC * 1.8 +32;
-	return tempF + " Faranhajt";
+	return tempF + ' Faranhajt';
 };
 
 
 function waterStatusC(temp) {
-	if (temp < 0) {return "woda jest w stanie stałym"}
-	else if (temp > 0 && temp < 100) {return "woda jest w stanie płynnym"}
-	else {return "woda jest w stanie lotnym"}
+	if (temp < 0) return 'woda jest w stanie stałym'
+	else if (temp > 0 && temp < 100) return 'woda jest w stanie płynnym'
+	else return 'woda jest w stanie lotnym'
 };
 
 function waterStatusF(temp) {
@@ -25,11 +25,11 @@ function waterStatusF(temp) {
 
 function FtoC(tempF) {
 	var tempC = ( tempF - 32 ) / 1.8;
-	return tempC + " Celsjusz"
+	return tempC + ' Celsjusz'
 };
 
 function showResult(info) {
-	resultBox.innerHTML = "<div>" + info + "</div>" + "<br>" + resultBox.innerHTML;
+	resultBox.innerHTML = '<div>' + info + '</div>' + '<br>' + resultBox.innerHTML;
 };
 
 
@@ -39,7 +39,7 @@ btnCtoF.addEventListener('click', function(){
 	var tempC = prompt('Proszę podać jaką wartość przeliczyć na stopnie Faranhajt?');
 
 	if ( !isNaN(parseInt(tempC) )) {
-		showResult(CtoF(tempC) + " " + waterStatusC(tempC));	
+		showResult(CtoF(tempC) + ' ' + waterStatusC(tempC));	
 	} else {
 		showResult('proszę o podanie wartości liczbowej');
 	}
@@ -52,7 +52,7 @@ btnFtoC.addEventListener('click', function(){
 	var tempF = prompt('Proszę podać jaką wartość przeliczyć na stopnie Faranhajt?');
 
 	if ( !isNaN(parseInt(tempF) )) {
-		showResult(FtoC(tempF) + " " + waterStatusF(tempF));	
+		showResult(FtoC(tempF) + ' ' + waterStatusF(tempF));	
 	} else {
 		showResult('proszę o podanie wartości liczbowej');
 	}
